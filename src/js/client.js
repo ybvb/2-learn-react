b4b3c089bb24a600b272f66240ca1a363ffd8798
ybvb <ybvb@users.noreply.github.com>
2017-04-22 16:31:53 +0200

    end of video #7

--- src/js/client.js
+++ src/js/client.js
@@ -1,7 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Router, Route, IndexRoute, browserHistory } from "react-router";
 
+import Bootstrap from "./vendor/bootstrap-without-jquery";
+
+import Archives from "./pages/Archives";
+import Featured from "./pages/Featured";
 import Layout from "./pages/Layout";
+import Settings from "./pages/Settings";
 
 const app = document.getElementById('app');
-ReactDOM.render(<Layout/>, app);
+
+ReactDOM.render(
+  <Router history={browserHistory}>
+    <Route path="/" component={Layout}>
+      <IndexRoute component={Featured}></IndexRoute>
+      <Route path="archives(/:article)" name="archives" component={Archives}></Route>
+      <Route path="settings" name="settings" component={Settings}></Route>
+    </Route>
+
+
+  </Router>
+, app);
